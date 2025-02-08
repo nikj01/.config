9a87572cba33f2d7b5d171d679f7b3a6a82878d2
nik <nik.jurawlyow2001@gmail.com>
2025-02-08 15:40:48 +0200

    cursor focus removed

---
+++ JetBrains/WebStorm2024.3/consoles/.history/db/8f135028-236e-4716-8571-f1a23c9e675c.sql
@@ -0,0 +1,7 @@
+select * from people;
+;-- -. . -..- - / . -. - .-. -.--
+select * from people.public."Person";
+;-- -. . -..- - / . -. - .-. -.--
+select * from person;
+;-- -. . -..- - / . -. - .-. -.--
+select * from "Person";
